0ee72ae83ef43cbd57c9a3600ee99ff2dadb2a33
Teo Radolovict <tr54427@fer.hr>
2024-01-23 19:09:22 +0100

    fixed linter errors

--- 1_lab/lab_1_consumer/lab_1_consumer.py
+++ 1_lab/lab_1_consumer/lab_1_consumer.py
@@ -1,6 +1,6 @@
 import json
 from typing import List
-from azure.eventhub import EventHubConsumerClient, PartitionContext, EventData
+from azure.eventhub import EventHubConsumerClient, EventData
 from datetime import datetime
 from azure.storage.filedatalake import DataLakeServiceClient
 from azure.core.exceptions import ResourceNotFoundError
@@ -8,14 +8,20 @@ from azure.core.exceptions import ResourceNotFoundError
 connection_str = "Endpoint=sb://tr-ehns-tpiuo.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ALnbtW1QVgn/2FDqdM436WSSoW8/cL5dX+AEhMUQA28="
 eventhub_name = "tr-eh-tpiou"
 consumer_group = "$Default"
-
 storage_account_name = "trredditstorage"
 storage_container_name = "trredditcontainer"
 storage_account_connection_str = "DefaultEndpointsProtocol=https;AccountName=trredditstorage;AccountKey=Xvd5NsR3V6YSo68+CY2kRSpR08qKCpYjsuJ1KG4M+85FfNZ234n/2cEUGg39WBWCeFE1Pkgpem/B+AStgf2m4w==;EndpointSuffix=core.windows.net"
-data_lake_service_client = DataLakeServiceClient.from_connection_string(conn_str=storage_account_connection_str)
-file_system_client = data_lake_service_client.get_file_system_client(file_system=storage_container_name)
 
-def on_event_batch(partition_context: PartitionContext, events: List[EventData]):
+data_lake_service_client = DataLakeServiceClient.from_connection_string(
+    conn_str=storage_account_connection_str
+)
+
+file_system_client = data_lake_service_client.get_file_system_client(
+    file_system=storage_container_name
+)
+
+
+def on_event_batch(pc, events: List[EventData]):
     file_counter = 0
     for event in events:
         post = json.loads(event.body_as_str(encoding='UTF-8'))
@@ -38,11 +44,10 @@ def on_event_batch(partition_context: PartitionContext, events: List[EventData])
         file_client.flush_data(len(event_bytes))
         file_counter += 1
 
+
 if __name__ == "__main__":
     event_hub_consumer_client = EventHubConsumerClient.from_connection_string(
         connection_str, consumer_group, eventhub_name=eventhub_name
     )
     with event_hub_consumer_client:
-        event_hub_consumer_client.receive_batch(
-            on_event_batch=on_event_batch
-        )
+        event_hub_consumer_client.receive_batch(on_event_batch=on_event_batch)
--- 1_lab/lab_1_producer/lab_1_producer.py
+++ 1_lab/lab_1_producer/lab_1_producer.py
@@ -5,32 +5,25 @@ from azure.eventhub import EventHubProducerClient, EventData, EventDataBatch
 
 connection_str = "Endpoint=sb://tr-ehns-tpiuo.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ALnbtW1QVgn/2FDqdM436WSSoW8/cL5dX+AEhMUQA28="
 eventhub_name = "tr-eh-tpiou"
-
 url = "https://oauth.reddit.com/r/dataengineering/top.json?limit=10&t=all"
 auth = requests.auth.HTTPBasicAuth(
-    "XlZ4O7CZJiuhBA4m46D9yw",
-    "F2UQF2peItFRzFjaa2DLEULPLa2HUw"
+    "XlZ4O7CZJiuhBA4m46D9yw", "F2UQF2peItFRzFjaa2DLEULPLa2HUw"
 )
 data = {
     "grant_type": "password",
     "username": "ProfessionalStyle409",
     "password": "nagzu74$.,"
 }
-headers = {
-    "User-agent": "tr:producer_app:v1.0 (by /u/Significant-Deal2472)"
-}
-
+headers = {"User-agent": "tr:producer_app:v1.0 (by /u/Significant-Deal2472)"}
 token_response = requests.post(
-    "https://www.reddit.com/api/v1/access_token", 
-    auth=auth,
-    data=data,
-    headers=headers
+    "https://www.reddit.com/api/v1/access_token", auth=auth, data=data, headers=headers
 )
 token = token_response.json()["access_token"]
 headers["Authorization"] = f"bearer {token}"
-     
+
+
 def add_to_batch(batch: EventDataBatch, after):
-    params = { "after": after, "limit": 10 }
+    params = {"after": after, "limit": 10}
     response = requests.get(url, headers=headers, params=params)
     print("Get response:")
     print(response)
@@ -41,6 +34,7 @@ def add_to_batch(batch: EventDataBatch, after):
         after = data["data"]["after"]
         return after
     return None
+
 
 async def run():
     producer_client = EventHubProducerClient.from_connection_string(

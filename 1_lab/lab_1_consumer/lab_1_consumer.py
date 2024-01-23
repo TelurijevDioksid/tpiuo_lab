cd6ca04f6983ddbd2237f3e722c7ab861ba2117d
Teo Radolovict <tr54427@fer.hr>
2024-01-23 14:56:38 +0100

    added data lake logic

--- 1_lab/lab_1_consumer/lab_1_consumer.py
+++ 1_lab/lab_1_consumer/lab_1_consumer.py
@@ -1,21 +1,49 @@
 import json
 
-from azure.eventhub import EventHubConsumerClient
+from typing import List
+from azure.eventhub import EventHubConsumerClient, PartitionContext, EventData
+from datetime import datetime
+from azure.storage.filedatalake import DataLakeServiceClient
+from azure.core.exceptions import ResourceNotFoundError
 
 connection_str = "Endpoint=sb://tr-ehns-tpiuo.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ALnbtW1QVgn/2FDqdM436WSSoW8/cL5dX+AEhMUQA28="
 eventhub_name = "tr-eh-tpiou"
 consumer_group = "$Default"
 
-def on_event(partition_context, event):
-    json_data = json.loads(event.body_as_str())
-    print(json_data)
-    partition_context.update_checkpoint(event)
+storage_account_name = "trredditstorage"
+storage_container_name = "trredditcontainer"
+storage_account_connection_str = "DefaultEndpointsProtocol=https;AccountName=trredditstorage;AccountKey=Xvd5NsR3V6YSo68+CY2kRSpR08qKCpYjsuJ1KG4M+85FfNZ234n/2cEUGg39WBWCeFE1Pkgpem/B+AStgf2m4w==;EndpointSuffix=core.windows.net"
+data_lake_service_client = DataLakeServiceClient.from_connection_string(conn_str=storage_account_connection_str)
+file_system_client = data_lake_service_client.get_file_system_client(file_system=storage_container_name)
 
-consumer = EventHubConsumerClient.from_connection_string(
-    connection_str, 
-    consumer_group=consumer_group, 
-    eventhub_name=eventhub_name
-)
+def on_event_batch(partition_context: PartitionContext, events: List[EventData]):
+    file_counter = 0
+    for event in events:
+        post = json.loads(event.body_as_str(encoding='UTF-8'))
+        creation = post["data"]["created_utc"]
+        creation_date = datetime.utcfromtimestamp(creation)
+        bucket_client = file_system_client.get_directory_client(
+            f"{creation_date.year}/{creation_date.month}/{creation_date.day}/{creation_date.hour}/{creation_date.minute}"
+        )
+        try:
+            dir_props = bucket_client.get_directory_properties()
+            print("Found bucket: " + dir_props.name)
+        except ResourceNotFoundError:
+            bucket_client.create_directory()
+            new_bucket = bucket_client.get_directory_properties()
+            print("New bucket: " + new_bucket.name)
+        file_client = bucket_client.get_file_client(str(file_counter))
+        file_client.create_file()
+        event_bytes = str(event).encode("utf-8")
+        file_client.append_data(data=event_bytes, offset=0, length=len(event_bytes))
+        file_client.flush_data(len(event_bytes))
+        file_counter += 1
 
-with consumer:
-    consumer.receive(on_event=on_event)
+if __name__ == "__main__":
+    event_hub_consumer_client = EventHubConsumerClient.from_connection_string(
+        connection_str, consumer_group, eventhub_name=eventhub_name
+    )
+    with event_hub_consumer_client:
+        event_hub_consumer_client.receive_batch(
+            on_event_batch=on_event_batch
+        )
--- 1_lab/lab_1_producer/lab_1_producer.py
+++ 1_lab/lab_1_producer/lab_1_producer.py
@@ -1,9 +1,11 @@
 import requests
 import json
-from azure.eventhub import EventHubProducerClient, EventData
+import asyncio
+from azure.eventhub import EventHubProducerClient, EventData, EventDataBatch
 
 connection_str = "Endpoint=sb://tr-ehns-tpiuo.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ALnbtW1QVgn/2FDqdM436WSSoW8/cL5dX+AEhMUQA28="
 eventhub_name = "tr-eh-tpiou"
+
 url = "https://oauth.reddit.com/r/dataengineering/top.json?limit=10&t=all"
 auth = requests.auth.HTTPBasicAuth(
     "XlZ4O7CZJiuhBA4m46D9yw",
@@ -27,15 +29,36 @@ token_response = requests.post(
 token = token_response.json()["access_token"]
 headers["Authorization"] = f"bearer {token}"
 
-posts_response = requests.get(url, headers=headers)
-data = posts_response.json()
+def add_to_batch(batch: EventDataBatch, after):
+    params = { "after": after, "limit": 10 }
+    response = requests.get(url, headers=headers, params=params)
+    print("Get response:")
+    print(response)
+    if response.ok:
+        data = response.json()
+        for post in data["data"]["childern"]:
+            batch.add(EventData(json.dumps(post).encode("utf-8")))
+        after = data["data"]["after"]
+        return after
+    return None
 
-producer = EventHubProducerClient.from_connection_string(connection_str, eventhub_name=eventhub_name)
+async def run():
+    producer_client = EventHubProducerClient.from_connection_string(
+        conn_str=connection_str, eventhub_name=eventhub_name
+    )
+    after = None
+    for _ in range(100):
+        batch = producer_client.create_batch()
+        after = add_to_batch(batch, after)
+        print("Batch:")
+        print(batch)
+        if not after:
+            break
+        producer_client.send_batch(batch)
+        await asyncio.sleep(10)
 
-event_data_batch = producer.create_batch()
-for post in data["data"]["children"]:
-    event_data_batch.add(EventData(json.dumps(post["data"])))
-producer.send_batch(event_data_batch)
 
-while True:
-    pass
+if __name__ == "__main__":
+    asyncio.run(run())
+    while True:
+        continue
